f84254a9569847bd2e1d42961ad8bfce8223d469
abmMattos <abmmattos0209@gmail.com>
2024-06-27 13:04:48 -0300

    fix status error on workstation with mock data on new date

--- web/src/pages/workstations/workstations.jsx
+++ web/src/pages/workstations/workstations.jsx
@@ -10,102 +10,108 @@ import { createColumnHelper } from "@tanstack/react-table"
 import axios from "axios"
 
 export function Workstations() {
-  const [workstation, setWorkstation] = useState([]);
-  const [reservation, setReservation] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const userType = localStorage.getItem('userType');
+    const [workstation, setWorkstation] = useState([]);
+    const [reservation, setReservation] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const userType = localStorage.getItem('userType');
 
-  const [open, setOpen] = useState(false);
-  if (open === true) {
-    document.body.style.overflow = 'hidden';
-  } else {
-    document.body.style.overflow = '';
-  }
-
-  const columnHelper = createColumnHelper();
-
-  const columns = [
-    columnHelper.accessor("index", {
-      cell: (info) => info.getValue(),
-      header: () => <strong>#</strong>,
-    }),
-    columnHelper.accessor((row) => row.status, {
-      id: "status",
-      cell: (info) => <i>{info.getValue()}</i>,
-      header: () => <strong>Status</strong>,
-    }),
-    columnHelper.accessor("description", {
-      header: () => <strong>Descrição</strong>,
-      cell: (info) => info.renderValue(),
-    }),
-    columnHelper.accessor("local", {
-      header: () => <strong>Localização</strong>,
-    }),
-  ];
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('https://workstation-management.onrender.com/workstation/');
-        setWorkstation(response.data);
-        setLoading(false);
-      } catch (error) {
-        setError(error);
-        setLoading(false);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          "https://workstation-management.onrender.com/reservation/findReservedMeetingRoom"
-        );
-        setReservation(response.data);
-      } catch (error) {
-        console.error('Não foi possível consultar salas');
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  let data = workstation.map(room => {
-    const date = new Date()
-    date.setUTCHours(0,0,0,0);
-    if(date === reservation.dateReserve) {
-      return { ...room, status: 'Indisponível'}
+    const [open, setOpen] = useState(false);
+    if (open === true) {
+        document.body.style.overflow = 'hidden';
+    } else {
+        document.body.style.overflow = '';
     }
-    return { ...room, status: 'Disponível'}
-  })
 
-  if (loading) {
-    return <Center><Spinner /></Center>;
-  }
+    const columnHelper = createColumnHelper();
 
-  if (error) {
-    return <div>Error fetching data</div>;
-  }
+    const columns = [
+        columnHelper.accessor("index", {
+            cell: (info) => info.getValue(),
+            header: () => <strong>#</strong>,
+        }),
+        columnHelper.accessor((row) => row.status, {
+            id: "status",
+            cell: (info) => <i>{info.getValue()}</i>,
+            header: () => <strong>Status</strong>,
+        }),
+        columnHelper.accessor("description", {
+            header: () => <strong>Descrição</strong>,
+            cell: (info) => info.renderValue(),
+        }),
+        columnHelper.accessor("local", {
+            header: () => <strong>Localização</strong>,
+        }),
+    ];
 
-  return (
-    <Main>
-      <Side />
-      <Section>
-        <Header title="Estações de Trabalho" />
-        <Section>
-          {userType === 'ADMIN' && (
-            <>
-              <AddButton id="addButton" click={() => setOpen(!open)} text="Nova Estação" img={plus} />
-              <NewWorkstationModal isOpen={open} setOpen={setOpen} />
-            </>
-          )}
-          <Table type="Workstation" dataTable={data} dataColumns={columns} url={'https://workstation-management.onrender.com/workstation/delete'} />
-        </Section>
-      </Section>
-    </Main>
-  )
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('https://workstation-management.onrender.com/workstation/');
+                setWorkstation(response.data);
+                setLoading(false);
+            } catch (error) {
+                setError(error);
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(
+                    "https://workstation-management.onrender.com/reservation/findReservedMeetingRoom"
+                );
+                setReservation(response.data);
+            } catch (error) {
+                console.error('Não foi possível consultar salas');
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    let data = workstation.map(work => {
+        let date = new Date("2024-06-27T00:00:00.000Z")
+        date = date.toISOString()
+        const [reserved] = reservation.map(res => {
+            if (work.id === res.workstation_id) {
+                return res;
+            }
+            return false
+        })
+        if(reserved.dateReserve === date) {
+            return { ...work, status: 'Indisponível' }
+        }
+        return { ...work, status: 'Disponível' }
+    })
+
+    if (loading) {
+        return <Center><Spinner /></Center>;
+    }
+
+    if (error) {
+        return <div>Error fetching data</div>;
+    }
+
+    return (
+        <Main>
+            <Side />
+            <Section>
+                <Header title="Estações de Trabalho" />
+                <Section>
+                    {userType === 'ADMIN' && (
+                        <>
+                            <AddButton id="addButton" click={() => setOpen(!open)} text="Nova Estação" img={plus} />
+                            <NewWorkstationModal isOpen={open} setOpen={setOpen} />
+                        </>
+                    )}
+                    <Table type="Workstation" dataTable={data} dataColumns={columns} url={'https://workstation-management.onrender.com/workstation/delete'} />
+                </Section>
+            </Section>
+        </Main>
+    )
 }

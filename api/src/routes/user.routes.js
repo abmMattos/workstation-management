b02d34518610b96fa82437015f7b3e258657b9aa
abmMattos <abmmattos0209@gmail.com>
2024-11-23 18:36:34 -0300

    fix: add role on user routes

--- api/src/routes/user.routes.js
+++ api/src/routes/user.routes.js
@@ -7,12 +7,12 @@ const userRoutes = Router()
 
 const userController = new UserController()
 
-userRoutes.post('/create', verifyToken, userController.create)
-userRoutes.post('/update', verifyToken, userController.update)
+userRoutes.post('/create', verifyToken(['admin']), userController.create)
+userRoutes.post('/update', verifyToken(['admin']), userController.update)
 userRoutes.get('/login', userController.login)
-userRoutes.get('/', userController.findMany)
-userRoutes.delete('/delete', verifyToken, userController.delete)
-userRoutes.get('/findunique', userController.findUnique)
+userRoutes.get('/', verifyToken(['admin', 'user']), userController.findMany)
+userRoutes.delete('/delete', verifyToken(['admin']), userController.delete)
+userRoutes.get('/findunique', verifyToken(['admin', 'user']), userController.findUnique)
 
 
 

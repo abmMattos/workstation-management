82f29412230902b613b4e6a1a0da7f6e27a3773b
abmMattos <abmmattos0209@gmail.com>
2024-11-23 18:45:12 -0300

    fix axiosConfig remove Bearer

--- web/src/axios/axiosConfig.js
+++ web/src/axios/axiosConfig.js
@@ -4,7 +4,7 @@ axios.interceptors.request.use(
     (config) => {
         const token = localStorage.getItem('token');
         if (token) {
-            config.headers['Authorization'] = `Bearer ${token}`;
+            config.headers['Authorization'] = `${token}`;
         }
         return config;
     },

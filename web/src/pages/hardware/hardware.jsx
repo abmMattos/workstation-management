7d7b6e8abddcb74b7e3c79f56ce0d1d70e57819d
abmMattos <abmmattos0209@gmail.com>
2024-11-23 15:54:13 -0300

    refact routes to insert auth token

--- web/src/pages/hardware/hardware.jsx
+++ web/src/pages/hardware/hardware.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
+import axios from "../../axios/axiosConfig.js";
 import { Center, Main, Section, Spinner } from "./hardwareStyle";
 import { Side } from "../../components/Side/side";
 import { AddButton } from "../../components/Button/Button";

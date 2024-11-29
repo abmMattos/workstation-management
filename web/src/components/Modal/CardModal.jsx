8131c3ed63698dad92bca5fa596c92119c83b765
Gabriel da Silva Homem <hsleirbag@hotmail.com>
2024-11-29 16:00:58 -0300

    style: change input text

--- web/src/components/Modal/CardModal.jsx
+++ web/src/components/Modal/CardModal.jsx
@@ -3,9 +3,8 @@ import { Checkbox, Input, Main } from "./CardModalStyle";
 export function CardModal(props) {
         return (
             <Main>
-                <label htmlFor={props.name}>{props.text}</label>
+                <label htmlFor={props.name}>{props.hasGuests === true ? "Escreva o motivo e horário de início:" :  props.text}</label>
                 <Input onChange={props.change} type={props.type} name={props.name} required={props.required ?? false}  value={props.value ? props.value : ""}/>
-                {props.hasGuests === true && (<small>Escreva o motivo e horário de início</small>)}
             </Main>
         )
 }
